0ea7507d6c90365568aed170f3fc78dd7f60e0b6
Izabela <izabelaszeremeta@gmail.com>
2022-12-08 19:02:18 -0500

    Complete steps 1-3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
-
 /*---------------------------- Variables (state) ----------------------------*/
+// let board = [null, null, null, null, null, null, null, null, null]
+// let turn = 1
+// let winner = false 
+// let tie = false
 
+let board, turn, winner, tie
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.getElementById("message")
 
+console.dir(squareEls);
 
+console.log(messageEl);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -16,3 +24,42 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+function init(){
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = false 
+  tie = false
+  render()
+}
+init()
+
+console.log(board);
+
+function render(){
+  updateBoard()
+  updateMessage()
+}
+
+function updateBoard() {
+  board.forEach(function(square, idx){
+    if (square === 1) {
+      squareEls[idx].textContent = "X"
+   } else if (square === -1) {
+      squareEls[idx].textContent = "O"
+   } else {
+      squareEls[idx].textContent = ""
+   }
+})
+}
+updateBoard()
+
+
+function updateMessage(){
+  if (winner === false && tie === false) {
+    msg = `Your turn ${winner}`
+  } else if (winner === false && tie === true) { 
+    msg = `You tied!`
+  } else {
+    msg = `${winner} has won!`
+  }
+}
