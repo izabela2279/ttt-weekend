ebb4f9cc247294de6ad1d130cd5d33c325d16694
Izabela <izabelaszeremeta@gmail.com>
2022-12-09 15:20:02 -0500

    Complete step 5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
+const winningCombos = [
+	[0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 // let board = [null, null, null, null, null, null, null, null, null]
@@ -9,18 +19,17 @@
 
 let board, turn, winner, tie
 
-
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll(".sqr")
 const messageEl = document.getElementById("message")
 
-console.dir(squareEls);
-
+console.log(squareEls);
 console.log(messageEl);
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
+squareEls.forEach(function(square){
+  square.addEventListener("click", handleClick)
+})
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -33,7 +42,7 @@ function init(){
 }
 init()
 
-console.log(board);
+console.log(init);
 
 function render(){
   updateBoard()
@@ -44,11 +53,11 @@ function updateBoard() {
   board.forEach(function(square, idx){
     if (square === 1) {
       squareEls[idx].textContent = "X"
-   } else if (square === -1) {
+    } else if (square === -1) {
       squareEls[idx].textContent = "O"
-   } else {
+    } else {
       squareEls[idx].textContent = ""
-   }
+    }
 })
 }
 
@@ -62,4 +71,31 @@ function updateMessage(){
     messageEl.textContent = `${turn} has won!`
   }
 }
+
+
+function handleClick(evt){
+  sqIdx = evt.target.id.slice(2)
+    if (board[sqIdx] !== null) {
+      return
+    } if (winner === true){
+      return
+    }
+  placePiece(sqIdx)
+  checkForTie()
+} 
+
+function placePiece(idx){
+  board[idx] = turn
+}
+
+function checkForTie(){
+  let check = board.every(function(square){
+    return square !== null
+  })
+  console.log(board);
+  console.log(check);
+}
+
+
+
 
